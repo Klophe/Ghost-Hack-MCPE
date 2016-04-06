839902b757ac2b82f24e7cba2f3a1d694000759d
KillerBLS <dm.mironov01@gmail.com>
2016-04-06 23:37:31 +0300

    Delete Coordinates.js

--- Coordinates.js
+++
@@ -1,7 +0,0 @@
-function modTick()
-{
-var px = getPlayerX();
-var py = getPlayerY();
-var pz = getPlayerZ();
-ModPE.showTipMessage("\n\n\n" + "X: " + parseInt(px) + " Y: " + parseInt(py) + " Z: " + parseInt(pz));
-}
